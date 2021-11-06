dec285f947e86cd9a578d72bcd83e780a6856949
Jeremias Marinho <jeremias10@msn.com>
2021-11-06 03:15:24 -0300

    commit backend

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,18 @@
-import { Router, Request, Response, response } from 'express';
+import { Router, Request, Response } from 'express';
+
+import { getTasks, saveTask, getTask, updateTask, finishedTask } from './controller/TasksControllers';
 
 const routes = Router();
 
 routes.get('/', (request: Request, response: Response) => {
-    return response.json({message: 'Hello World'})
+    return response.json({message: 'Hello World3'})
   })
+
+  routes.get('/tasks', getTasks)
+  routes.get('/tasks/:id', getTask)
+  routes.post('/tasks', saveTask)
+  routes.put('/tasks/:id', updateTask)
+  routes.patch('/tasks/:id', finishedTask)
+  
 
   export default routes;
